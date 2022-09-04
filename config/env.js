cf8bbc836dfbf5a39b22bb37e538e6b2edea2aae
Oleg Selivantsev <o.selivancev@gmail.com>
2022-09-04 12:45:34 +0300

    env upd

---
+++ config/env.js
@@ -0,0 +1,4 @@
+const {API_PORT} = process.env;
+const port = API_PORT || 3000;
+
+module.export = {port}
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
-
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
+const {port} = require("./config/env");
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
